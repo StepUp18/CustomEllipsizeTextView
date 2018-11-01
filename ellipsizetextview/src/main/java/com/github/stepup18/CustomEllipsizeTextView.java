783e42138040f720ff6adb805d8590a4bba0ff52
StepUp18 <dsp@htc-cs.ru>
2018-11-01 17:12:58 +0400

    Edits x2

--- ellipsizetextview/src/main/java/com/github/stepup18/CustomEllipsizeTextView.java
+++ ellipsizetextview/src/main/java/com/github/stepup18/CustomEllipsizeTextView.java
@@ -1,6 +1,5 @@
 package com.github.stepup18;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatTextView;
@@ -17,13 +16,12 @@ import java.util.List;
 
 public class CustomEllipsizeTextView extends AppCompatTextView {
 
-    public static final int UNIVERSAL_MEASURE_POSITION_VALUE = 1073741824;
-    private CharSequence mEllipsizeText;
-    private CharSequence mOriginText;
-    private int mEllipsizeIndex;
-    private int mMaxLines;
-    private boolean mIsExactlyMode;
-    private boolean mEnableUpdateOriginText;
+    private CharSequence ellipsizeText;
+    private CharSequence originalText;
+    private int ellipsizeIndex;
+    private int maxLines;
+    private boolean isExactlyMode;
+    private boolean enableUpdateOriginText;
 
     public CustomEllipsizeTextView(Context context) {
         this(context, null);
@@ -31,29 +29,28 @@ public class CustomEllipsizeTextView extends AppCompatTextView {
 
     public CustomEllipsizeTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.mEnableUpdateOriginText = true;
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CustomEllipsizeTextView);
-        this.mEllipsizeIndex = ta.getInt(R.styleable.CustomEllipsizeTextView_ellipsizeIndex, 0);
-        this.mEllipsizeText = ta.getText(R.styleable.CustomEllipsizeTextView_ellipsizeText);
-        if (this.mEllipsizeText == null) {
-            this.mEllipsizeText = "...";
+        this.enableUpdateOriginText = true;
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomEllipsizeTextView);
+        this.ellipsizeIndex = typedArray.getInt(R.styleable.CustomEllipsizeTextView_ellipsizeIndex, 0);
+        this.ellipsizeText = typedArray.getText(R.styleable.CustomEllipsizeTextView_ellipsizeText);
+        if (this.ellipsizeText == null) {
+            this.ellipsizeText = "...";
         }
-        ta.recycle();
+        typedArray.recycle();
     }
 
     public void setMaxLines(int maxLines) {
-        if (this.mMaxLines != maxLines) {
+        if (this.maxLines != maxLines) {
             super.setMaxLines(maxLines);
-            this.mMaxLines = maxLines;
+            this.maxLines = maxLines;
         }
     }
 
-    @SuppressLint("WrongConstant")
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        this.setText(this.mOriginText);
+        this.setText(this.originalText);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         try {
-            this.mIsExactlyMode = MeasureSpec.getMode(widthMeasureSpec) == UNIVERSAL_MEASURE_POSITION_VALUE;
+            this.isExactlyMode = MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.EXACTLY;
             Layout layout = this.getLayout();
             if (layout != null && (this.isExceedMaxLine(layout) || this.isOutOfBounds(layout))) {
                 this.adjustEllipsizeEndText(layout);
@@ -66,17 +63,17 @@ public class CustomEllipsizeTextView extends AppCompatTextView {
     }
 
     public void setText(CharSequence text, BufferType type) {
-        if (this.mEnableUpdateOriginText) {
-            this.mOriginText = text;
+        if (this.enableUpdateOriginText) {
+            this.originalText = text;
         }
         super.setText(text, type);
-        if (this.mIsExactlyMode) {
+        if (this.isExactlyMode) {
             this.requestLayout();
         }
     }
 
     private boolean isExceedMaxLine(Layout layout) {
-        return layout.getLineCount() > this.mMaxLines && this.mMaxLines > 0;
+        return layout.getLineCount() > this.maxLines && this.maxLines > 0;
     }
 
     private boolean isOutOfBounds(Layout layout) {
@@ -84,29 +81,29 @@ public class CustomEllipsizeTextView extends AppCompatTextView {
     }
 
     private void adjustEllipsizeEndText(Layout layout) {
-        String originText = ((String) this.mOriginText).replaceAll("\\n", " ");
-        CharSequence restSuffixText = originText.subSequence(originText.length() - this.mEllipsizeIndex, originText.length());
+        String originalText = ((String) this.originalText).replaceAll("\\n", " ");
+        CharSequence restSuffixText = originalText.subSequence(originalText.length() - this.ellipsizeIndex, originalText.length());
         int width = layout.getWidth() - this.getPaddingLeft() - this.getPaddingRight();
         int maxLineCount = Math.max(1, this.computeMaxLineCount(layout));
         if (maxLineCount < 2) {
             maxLineCount = 2;
         }
         int lastLineWidth = (int) layout.getLineWidth(maxLineCount - 1);
-        int mLastCharacterIndex = layout.getLineEnd(maxLineCount - 1);
-        int suffixWidth = (int) (Layout.getDesiredWidth(this.mEllipsizeText, this.getPaint()) + Layout.getDesiredWidth(restSuffixText, this.getPaint())) + 1;
-        this.mEnableUpdateOriginText = false;
+        int lastCharacterIndex = layout.getLineEnd(maxLineCount - 1);
+        int suffixWidth = (int) (Layout.getDesiredWidth(this.ellipsizeText, this.getPaint()) + Layout.getDesiredWidth(restSuffixText, this.getPaint())) + 1;
+        this.enableUpdateOriginText = false;
         if (lastLineWidth + suffixWidth > width) {
             int widthDiff = lastLineWidth + suffixWidth - width;
-            int removedCharacterCount = this.computeRemovedEllipsizeEndCharacterCount(widthDiff, originText.subSequence(0, mLastCharacterIndex));
-            this.setText(originText.subSequence(0, mLastCharacterIndex - removedCharacterCount));
-            this.append(this.mEllipsizeText);
+            int removedCharacterCount = this.computeRemovedEllipsizeEndCharacterCount(widthDiff, originalText.subSequence(0, lastCharacterIndex));
+            this.setText(originalText.subSequence(0, lastCharacterIndex - removedCharacterCount));
+            this.append(this.ellipsizeText);
             this.append(restSuffixText);
         } else {
-            this.setText(originText.subSequence(0, mLastCharacterIndex));
-            this.append(this.mEllipsizeText);
+            this.setText(originalText.subSequence(0, lastCharacterIndex));
+            this.append(this.ellipsizeText);
             this.append(restSuffixText);
         }
-        this.mEnableUpdateOriginText = true;
+        this.enableUpdateOriginText = true;
     }
 
     private int computeMaxLineCount(Layout layout) {
@@ -170,34 +167,37 @@ public class CustomEllipsizeTextView extends AppCompatTextView {
         }
     }
 
-    public void setEllipsizeText(CharSequence ellipsizeText, int ellipsizeIndex) {
-        this.mEllipsizeText = ellipsizeText;
-        this.mEllipsizeIndex = ellipsizeIndex;
+    public void setEllipsizeText(CharSequence ellipsizeText) {
+        this.ellipsizeText = ellipsizeText;
+    }
+
+    public void setEllipsizeIndex(int mEllipsizeIndex) {
+        this.ellipsizeIndex = mEllipsizeIndex;
     }
 
     public static final class Range<T extends Comparable<? super T>> {
-        private final T mLower;
-        private final T mUpper;
+        private final T lower;
+        private final T upper;
 
         public Range(T lower, T upper) {
-            this.mLower = lower;
-            this.mUpper = upper;
+            this.lower = lower;
+            this.upper = upper;
             if (lower.compareTo(upper) > 0) {
                 throw new IllegalArgumentException("lower must be less than or equal to upper");
             }
         }
 
         public T getLower() {
-            return this.mLower;
+            return this.lower;
         }
 
         public T getUpper() {
-            return this.mUpper;
+            return this.upper;
         }
 
         public boolean contains(T value) {
-            boolean gteLower = value.compareTo(this.mLower) >= 0;
-            boolean lteUpper = value.compareTo(this.mUpper) < 0;
+            boolean gteLower = value.compareTo(this.lower) >= 0;
+            boolean lteUpper = value.compareTo(this.upper) < 0;
             return gteLower && lteUpper;
         }
     }
